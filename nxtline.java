85ab45c8d0bac7b3427a7760e1049288ad4eba4b
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 12:01:49 +0530

    Add files via upload

---
+++ nxtline.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String args[])
+    {
+        Scanner n=new Scanner (System.in);
+        String a=n.next();
+        String b=n.next();
+        String c=n.next();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
