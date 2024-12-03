af11d8ffc32b9ccfa0506a4ba614f604f9243995
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 11:56:24 +0530

    Add files via upload

---
+++ printingnxtchar.java
@@ -0,0 +1,12 @@
+class B
+{
+    public static void main(String args[])
+    {
+        char c='A';
+        int B=c;
+        B+=1;
+        char v=(char)(B);
+        System.out.println(v);
+    }
+
+}
