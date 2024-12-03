6d1ea0b801593baea18780941e9355c692246eea
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 13:34:05 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,13 @@
+class D 
+{
+    public static void main(String args[])
+    {
+        int a=8;
+        float b=6.0;
+        char c='D';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
