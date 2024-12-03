b10a4ca1b88fe9f03f6affb5e819c6a86d7161db
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 13:31:12 +0530

    Add files via upload

---
+++ stringformat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class X
+{
+    public static void main(String args[])
+    {
+        Scanner n=new Scanner(System.in);
+        String a=n.next();
+        String b=String.format("congratulations %s you won",a);
+        System.out.println(b);
+
+    }
+}
